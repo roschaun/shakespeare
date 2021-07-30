0a0a5cfe5e1059ebed650ce07d926cdf0b5f3448
Roschaun Johnson <roschaun.johnson@mobilesmith.com>
2021-07-30 19:20:51 -0400

    Initial project checkin.

---
+++ Shakespeare/Shared/ShakespeareApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShakespeareApp.swift
+//  Shared
+//
+//  Created by Roschaun Johnson on 7/30/21.
+//
+
+import SwiftUI
+
+@main
+struct ShakespeareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
